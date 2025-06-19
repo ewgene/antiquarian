fc9c31f15969ef241d602f6b16602c4c862a19d4
ewgene <ewgene.shure@gmail..com>
2025-06-19 13:17:23 +0700

    requirements

--- antiquarian/settings.py
+++ antiquarian/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!7%(ecgd%+x0$5-h9e#2f)g!9$6+d_5d$rz$u))92-=tw!buqx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
