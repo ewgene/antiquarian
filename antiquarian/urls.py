db77471272e2960cf07c25e7b65ceee067dc881d
ewgene <ewgene.shure@gmail..com>
2025-06-25 10:28:07 +0700

    before first deploy

--- antiquarian/urls.py
+++ antiquarian/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings # type: ignore
+from django.conf.urls.static import static # type: ignore
 from django.contrib import admin # type: ignore
 from django.urls import path, include # type: ignore
 from products import views
@@ -6,4 +8,4 @@ from products import urls as products_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
